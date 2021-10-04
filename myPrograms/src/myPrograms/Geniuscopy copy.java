6f50576f9c7ac80ca71081f002e80d7c6cefcdfe
Tutorial <tutorial@Davids-MBP.attlocal.net>
2021-10-03 22:42:24 -0700

    new file

---
+++ myPrograms/src/myPrograms/Geniuscopy copy.java
@@ -0,0 +1,56 @@
+package myPrograms;
+
+public class Genius {
+
+	/* main Method Declaration */
+	public static void main(String[] args) {
+		gotNames(); //Call Method gotNames()
+		gotJokes(); //Call Method gotJokes()
+		gotCommunityNorms(); //Call Method gotCommunityNorms()
+		
+		/*Add Your Method Calls Here*/
+		
+	}
+
+	/* gotNames Method Declaration */
+	public static void gotNames() {
+
+		/* GENIUS NAMES - SAY OUR NAMES */
+		System.out.println("My Name Is David Malone Jr."); // Print Genius Name
+		System.out.println("My Name Is Perry Irving."); // Print Genius Name
+		System.out.println("My Name Is Kevin Butler."); // Print Genius Name
+		System.out.println("");//Print Empty Lines
+		System.out.println("");//Print Empty Lines
+	}
+
+	/* gotJokes Method Declaration */
+	public static void gotJokes() {
+
+		/* GENIUS JOKES - WE GOT JOKES */
+		System.out.println("Welcome, My Name Is David!"); // Print My Name
+		System.out.println("Why do rappers need umbrellas?"); // Joke Setup
+		System.out.println("Fo'Drizzle"); // Joke Punch Line
+		System.out.println(""); // Extra Line
+		System.out.println(""); // Extra Line
+
+	}
+
+	/* gotCommunityNorms Method Declaration */
+	public static void gotCommunityNorms() {
+		/* GENIUS COMMUNITY NORMS - KEEP US ACCOUNTABLE */
+		System.out.println("OUR COMMUNITY NORMS");
+		System.out.println("----------------------");
+		System.out.println("1. Community Norm 1");
+		System.out.println("2. Community Norm 2");
+		System.out.println("3. Community Norm 3");
+		System.out.println("4. Community Norm 4");
+		System.out.println("5. Community Norm 5");
+		System.out.println("----------------------");
+		System.out.println(""); // Extra Line
+		System.out.println(""); // Extra Line
+
+	}
+
+	/* YOUR METHODS START HERE */
+
+}
